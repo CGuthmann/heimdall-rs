3e0de4222e9b720cedb6e7cb111436674697f791
Claus Guthmann <claus.guthmann@web.de>
2023-07-31 09:40:52 -0500

    updated paths

--- ark-circom-service/build.rs
+++ ark-circom-service/build.rs
@@ -4,8 +4,11 @@ fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
 
     if target_os == "android" {
-        println!("cargo:rustc-link-search=native=/home/cguth/Vidar/backend/ark-circom-service/lib/aarch64-linux-android");
-        println!("cargo:rustc-link-search=native=/home/cguth/Android/Sdk/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/33");
+
+        let android_home = env::var("ANDROID_HOME").unwrap();
+
+        println!("cargo:rustc-link-search=native=./ark-circom-service/lib/aarch64-linux-android");
+        println!("cargo:rustc-link-search=native={}/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/33",android_home);
 
         println!("cargo:rustc-link-lib=dylib=c++");
         println!("cargo:rustc-link-lib=static=gmp");
@@ -13,7 +16,7 @@ fn main() {
 
         println!("cargo:rustc-link-lib=static=poseidon_bench");
     } else {
-        println!("cargo:rustc-link-search=native=/home/cguth/Vidar/backend/ark-circom-service/lib/x86_64-unknown-linux-gnu");
+        println!("cargo:rustc-link-search=native=./ark-circom-service/lib/x86_64-unknown-linux-gnu");
         println!("cargo:rustc-link-search=native=/usr/lib/gcc/x86_64-linux-gnu/10/"); //path to libstdc++.a
     
         println!("cargo:rustc-link-lib=static=stdc++");
--- heimdall/build.rs
+++ heimdall/build.rs
@@ -4,8 +4,11 @@ fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
 
     if target_os == "android" {
+
+        let android_home = env::var("ANDROID_HOME").unwrap();
+
         println!("cargo:rustc-link-search=native=../witnesscalc/package_android/lib");
-        println!("cargo:rustc-link-search=native=/home/cguth/Android/Sdk/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/33");
+        println!("cargo:rustc-link-search=native={}/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/33",android_home);
 
         println!("cargo:rustc-link-lib=dylib=c++");
         println!("cargo:rustc-link-lib=static=fr");
@@ -16,8 +19,8 @@ fn main() {
         println!("cargo:rustc-link-lib=static=presentation_polygon");
         println!("cargo:rustc-link-lib=static=presentation_delegation");
     } else {
-        println!("cargo:rustc-link-search=native=../witnesscalc/package/lib");
         println!("cargo:rustc-link-search=native=/usr/lib/gcc/x86_64-linux-gnu/10/"); //path to libstdc++.a
+        println!("cargo:rustc-link-search=native=./lib/");
 
         println!("cargo:rustc-link-lib=static=stdc++");
         println!("cargo:rustc-link-lib=static=fr");
