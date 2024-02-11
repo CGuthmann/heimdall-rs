b6b3ce04bf39d3de8f2c692ce92157afd8eaa3a2
claus.guthmann <claus.guthmann@fim-rc.de>
2024-02-11 17:36:09 +0000

    Fixed lib dependency

--- heimdall/build.rs
+++ heimdall/build.rs
@@ -9,23 +9,27 @@ fn main() {
 
         println!("cargo:rustc-link-search=native=../witnesscalc/package_android/lib");
         println!("cargo:rustc-link-search=native={}/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/33",android_home);
+        println!("cargo:rustc-link-search=native=/usr/lib/heimdall-libs/aarch64-linux-android/");
 
         println!("cargo:rustc-link-lib=dylib=c++");
         println!("cargo:rustc-link-lib=static=fr");
         println!("cargo:rustc-link-lib=static=gmp");
 
+        println!("cargo:rustc-link-search=native=/usr/lib/heimdall-libs/");
         println!("cargo:rustc-link-lib=static=presentation_attribute");
         println!("cargo:rustc-link-lib=static=presentation_range");
         println!("cargo:rustc-link-lib=static=presentation_polygon");
         println!("cargo:rustc-link-lib=static=presentation_delegation");
-    } else {
+    } else {        
+        println!("cargo:rustc-link-search=native=./ark-circom-service/lib/x86_64-unknown-linux-gnu");
         println!("cargo:rustc-link-search=native=/usr/lib/gcc/x86_64-linux-gnu/10/"); //path to libstdc++.a
-        println!("cargo:rustc-link-search=native=./heimdall/lib/x86_64-unknown-linux-gnu");
+        println!("cargo:rustc-link-search=native=/usr/lib/heimdall-libs/x86_64-unknown-linux-gnu/");
 
         println!("cargo:rustc-link-lib=static=stdc++");
         println!("cargo:rustc-link-lib=static=fr");
         println!("cargo:rustc-link-lib=static=gmp");
 
+        println!("cargo:rustc-link-search=native=/usr/lib/heimdall-libs/");
         println!("cargo:rustc-link-lib=static=presentation_attribute");
         println!("cargo:rustc-link-lib=static=presentation_range");
         println!("cargo:rustc-link-lib=static=presentation_polygon");
